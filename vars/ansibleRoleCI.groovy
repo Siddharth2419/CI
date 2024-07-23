71d03fe645e3cf05fd2ce7ca31bc152cdbab53ae
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-07-24 00:17:25 +0530

    Update ansibleRoleCI.groovy

--- vars/ansibleRoleCI.groovy
+++ vars/ansibleRoleCI.groovy
@@ -1,70 +1,19 @@
-import org.example.ansible.Checkout
-import org.example.ansible.Gitleaks
-import org.example.ansible.InstallGitleaks
-import org.example.ansible.AnsibleSyntaxCheck
-import org.example.ansible.LintCheck
+// ansibleLint.groovy
+def ansibleLint(String path) {
+    org.example.ansible.AnsibleLint.call(path)
+}
 
-def call(Map params) {
-    pipeline {
-        agent any
+// ansibleSyntaxCheck.groovy
+def ansibleSyntaxCheck(String path) {
+    org.example.ansible.AnsibleLint.call(path)
+}
 
-        environment {
-            GITHUB_TOKEN = credentials('github') // Ensure this matches your Jenkins credential ID
-            LC_ALL = 'en_US.UTF-8'
-            LANG = 'en_US.UTF-8'
-        }
+// dryRun.groovy
+def dryRun(String path) {
+    org.example.ansible.DryRun.call(path)
+}
 
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    script {
-                        new Checkout().call('https://github.com/Siddharth2419/CI.git', 'github-token', 'main')
-                    }
-                }
-            }
-
-            stage('Download and Install Gitleaks') {
-                steps {
-                    script {
-                        new InstallGitleaks().call()
-                    }
-                }
-            }
-
-            stage('Gitleaks Scan') {
-                steps {
-                    script {
-                        new Gitleaks().call()
-                    }
-                }
-            }
-
-            stage('Ansible Syntax Check') {
-                steps {
-                    script {
-                        new AnsibleSyntaxCheck().call(params.PLAYBOOK_NAME)
-                    }
-                }
-            }
-
-            stage('Build Env and Run Lint Check') {
-                steps {
-                    script {
-                        new LintCheck().call(params.PLAYBOOK_NAME)
-                    }
-                }
-            }
-        }
-
-        post {
-            success {
-                archiveArtifacts artifacts: '**/CredScanReport, **/lint_report.txt'
-                cleanWs()
-            }
-            failure {
-                archiveArtifacts artifacts: '**/CredScanReport, **/lint_report.txt'
-                cleanWs()
-            }
-        }
-    }
+// credScanning.groovy
+def credScanning() {
+    org.example.ansible.DryRun.call('')
 }
