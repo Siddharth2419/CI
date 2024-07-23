c3a4084a6fbc23ccc83bf95d1a2d058d494c5889
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-07-24 00:02:27 +0530

    Create AnsibleSyntxCheck.groovy

---
+++ src/org/example/ansible/AnsibleSyntxCheck.groovy
@@ -0,0 +1,9 @@
+package org.example.ansible
+def call(String path) {
+    stage('Ansible syntax-check') {
+        script {
+            echo "Checking the syntax of your ansible role";
+            sh "ansible-playbook ${path} --syntax-check"
+        }
+    }
+}
